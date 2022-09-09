8a161ed573b770ff47bc766f4b14f240b82b932f
vavu786 <hamzamian.patwa@gmail.com>
2022-09-09 13:06:45 -0500

    Simple user input test

--- Notebook_with_Jazz/src/Main.java
+++ Notebook_with_Jazz/src/Main.java
@@ -3,7 +3,10 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		Scanner s1 = new Scanner(System.in);
+		System.out.print("Enter a name: ");
+		String name = s1.nextLine();
+		System.out.println("Hello, " + name + ".");
 
 	}
 
